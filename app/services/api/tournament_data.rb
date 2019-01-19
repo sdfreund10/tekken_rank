1c14e4b5215718ea907faace464ecc6808b4b499
Stephen Freund <sdfreund10@gmail.com>
2019-01-19 11:52:56 -0500

    Filter API results on tekken id

--- app/services/api/tournament_data.rb
+++ app/services/api/tournament_data.rb
@@ -1,26 +1,48 @@
 # frozen_string_literal: true
 
-require "open-uri"
+require 'open-uri'
 
 class API::TournamentData
-  BASE_URL = "https://api.smash.gg/".freeze
-
-  def initialize(tournament_name, href)
+  BASE_URL = 'https://api.smash.gg/'
+  # find phase groups for the right game and get sets
+  def initialize(_tournament_name, href)
     @href = href
   end
 
   def data
-    @data ||= api_data = JSON.parse(open(BASE_URL + @href).read)
+    @data ||= JSON.parse(
+      open(
+        BASE_URL + @href + '?expand[]=event&expand[]=groups&expand[]=phase'
+      ).read
+    )
   end
 
   def tournament_data
-    data.dig("entities", "tournament")
+    data.dig('entities', 'tournament')
+  end
+
+  def tekken_events
+    data.dig('entities', 'event').select { |event| event['videogameId'] == 17 }
+  end
+
+  def tekken_phases
+    tekken_event_ids = tekken_events.map { |event| event['id'] }
+    data.dig('entities', 'phase').select do |phase|
+      tekken_event_ids.include? phase['eventId']
+    end
+  end
+
+  def tekken_groups
+    tekken_phase_ids = tekken_phases.map { |phase| phase['id'] }
+    data.dig('entities', 'groups').select do |group|
+      tekken_phase_ids.include? group['phaseId']
+    end
   end
 
   def self.tournament_metadata
     tournaments.each do |base_data|
       name, href, date = base_data.values_at(:name, :href, :date)
-      new(name, href)  
+      new(name, href)
     end
   end
 
@@ -28,4 +50,3 @@ class API::TournamentData
     TournamentPuller.new.all_tournament_codes
   end
 end
-
--- test/services/api/tournament_data_test.rb
+++ test/services/api/tournament_data_test.rb
@@ -3,6 +3,7 @@
 require 'test_helper'
 require 'mocha/minitest'
 require 'webmock/minitest'
+load Rails.root.join('test', 'helpers', 'tournament_reponse.rb')
 
 class API::TournamentDataTest < ActiveSupport::TestCase
   def setup
@@ -10,29 +11,68 @@ class API::TournamentDataTest < ActiveSupport::TestCase
     # makes lots of external api requests
     TournamentPuller.any_instance.stubs(:all_tournament_codes).returns(
       [
-        { name: "Tournament A",
-          href: "tournament/tournament-a",
-          date: "December 18th 2018" }
+        { name: 'Tournament A',
+          href: 'tournament/tournament-a',
+          date: 'December 18th 2018' }
       ]
     )
   end
 
   def stub_tournaments
     uri_template = Addressable::Template.new(
-      "https://api.smash.gg/tournament/{slug}"
+      'https://api.smash.gg/tournament/{slug}?'\
+        'expand[]=event&expand[]=groups&expand[]=phase'
     )
-    load Rails.root.join("test", "helpers", "tournament_reponse.rb")
     stub_request(:any, uri_template).to_return(
       body: TOURNAMENT_RESPONSE.to_json
     )
   end
 
-  test "#data fetches tournament data from api" do
+  test '#data fetches tournament data from api' do
     stub = stub_tournaments
-    scraper = API::TournamentData.new("Tournament A", "tournament/tournament-a")
+    scraper = API::TournamentData.new('Tournament A', 'tournament/tournament-a')
     scraper.data
 
     assert_requested(stub)
   end
-end
 
+  test '#tekken_events gets event data with tekken 7 id' do
+    stub_tournaments
+    scraper = API::TournamentData.new('Tournament A', 'tournament/tournament-a')
+    events = scraper.tekken_events
+    events.each do |event|
+      assert_equal(event['videogameId'], 17)
+      assert_match(/\Atournament\/.*\/event\/.*\z/, event['slug'])
+    end
+  end
+
+  test '#tekken_phases selects phases belonging to tekken events' do
+    stub = stub_tournaments
+    scraper = API::TournamentData.new('Tournament A', 'tournament/tournament-a')
+    phases = scraper.tekken_phases
+
+    phases.each do |phase|
+      # may only be true for test data
+      assert_equal phase['name'], 'Bracket'
+      assert_includes(
+        scraper.tekken_events.map { |ev| ev['id'] },
+        phase['eventId']
+      )
+    end
+  end
+
+  test '#tekken_groups selects groups belonging to tekken phases' do
+    stub = stub_tournaments
+    scraper = API::TournamentData.new('Tournament A', 'tournament/tournament-a')
+    groups = scraper.tekken_groups
+
+    groups.each do |group|
+      # may only be true for test data
+      assert_equal group['percentageComplete'], '1.0000'
+      assert_includes(
+        scraper.tekken_phases.map { |ev| ev['id'] },
+        group['phaseId']
+      )
+    end
+  end
+end
